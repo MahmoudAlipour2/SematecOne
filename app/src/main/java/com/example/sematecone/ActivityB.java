8ca32387fe7704a9958b1e23e61c969713d5275e
MahmoudAlipour <mahmoudalipour2@gmail.com>
2019-01-27 16:15:06 +0330

    Session3 Task1&2 -Sematec Android-LinearLayour&LifeCycle

---
+++ app/src/main/java/com/example/sematecone/ActivityB.java
@@ -0,0 +1,17 @@
+package com.example.sematecone;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+
+}
+
+
--- app/src/main/java/com/example/sematecone/MainActivity.java
+++ app/src/main/java/com/example/sematecone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sematecone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+//region Sematec Session 3 Task 1 : LifeCycle
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        Log.d("LifeCycleTest","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("LifeCycleTest","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d("LifeCycleTest","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d("LifeCycleTest","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d("LifeCycleTest","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d("LifeCycleTest","onRestart");
+    }
+    //endregion
+
+
+
+
 }
